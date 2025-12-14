64fa1c0f844a77244754fb2a53b385fd2b2b639a
Tori Hutto <183306074+hutto-dev@users.noreply.github.com>
2025-12-14 14:19:26 -0500

    Added first js, clicking topics

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // MENU
+  if (document.body.classList.contains("page-menu")) {
+    const topics = document.querySelectorAll(".topic");
+
+    topics.forEach((topic) => {
+      topic.addEventListener("click", () => {
+        const topicId = topic.id;
+        window.location.href = `./scripture.html?topic=${topicId}`;
+      });
+    });
+
+    // grab the topics
+    // foreach the topics
+    // add eventlistenr for a click
+    // when click happens, grab the topic id
+    // assign the topic id to the windows thing
+  }
+
+  // SCRIPTURE
+  if (document.body.classList.contains("page-scripture")) {
+  }
+});
